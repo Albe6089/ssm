167e598643e9fc80a615635e9c1e37d77eed2788
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-07 22:37:11 -0500

    configured backend

--- backend_tfstate.tf
+++ backend_tfstate.tf
@@ -1,13 +1,13 @@
 # Remote backend
-// terraform {
-//   backend "s3" {
-//     bucket = "bastion-tfstate-backend"
-//     key    = "env:/dev/bastion.tfstate"
-//     region = "us-east-1"
-//     dynamodb_table = "bastion-terraform-state-locking"
-//     encrypt = true
-//   }
-// }
+terraform {
+  backend "s3" {
+    bucket         = "bastion-tfstate-backend"
+    key            = "env:/dev/bastion.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "bastion-terraform-state-lock"
+    encrypt        = true
+  }
+}
 
 # Creating bucket for tfstate
 resource "aws_s3_bucket" "bucket" {
@@ -31,16 +31,16 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 # Creating Dynamodf for locking
-// resource "aws_dynamodb_table" "terraform_lock" {
-//   name           = "bastion-terraform-state-locking"
-//   read_capacity  = 5
-//   write_capacity = 5
-//   hash_key       = "LockID"
-//   attribute {
-//     name = "LockID"
-//     type = "S"
-//   }
-//   tags = {
-//     "Name" = "DynamoDB tfstate Lock Table"
-//   }
-// }
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "bastion-terraform-state-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    "Name" = "DynamoDB tfstate Lock Table"
+  }
+}
