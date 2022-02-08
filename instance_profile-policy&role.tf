b2a4c8584fab33eacc37a057e627eb6c306cbd84
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-07 23:15:45 -0500

    add instance profile, policy and role

---
+++ instance_profile-policy&role.tf
@@ -0,0 +1,98 @@
+resource "aws_iam_instance_profile" "default" {
+  name = "ec2_host_instance_profile"
+  role = aws_iam_role.default.name
+}
+
+resource "aws_iam_role" "default" {
+  name = "ec2_host_iam_role"
+  path = "/"
+
+  assume_role_policy = data.aws_iam_policy_document.default.json
+}
+
+resource "aws_iam_role_policy" "main" {
+  name   = "ec2_iam_role_policy"
+  role   = aws_iam_role.default.id
+  policy = data.aws_iam_policy_document.main.json
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy_document" "main" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ssm:DescribeAssociation",
+      "ssm:GetDeployablePatchSnapshotForInstance",
+      "ssm:GetDocument",
+      "ssm:DescribeDocument",
+      "ssm:GetManifest",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:ListAssociations",
+      "ssm:ListInstanceAssociations",
+      "ssm:PutInventory",
+      "ssm:PutComplianceItems",
+      "ssm:PutConfigurePackageResult",
+      "ssm:UpdateAssociationStatus",
+      "ssm:UpdateInstanceAssociationStatus",
+      "ssm:UpdateInstanceInformation"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2messages:AcknowledgeMessage",
+      "ec2messages:DeleteMessage",
+      "ec2messages:FailMessage",
+      "ec2messages:GetEndpoint",
+      "ec2messages:GetMessages",
+      "ec2messages:SendReply"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetEncryptionConfiguration"
+    ]
+
+    resources = ["*"]
+  }
+}
