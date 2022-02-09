861179e39912b70299e285ba2ae0919b6c3d1af2
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-09 00:00:47 -0500

    back track#3

--- outputs.tf
+++ outputs.tf
@@ -4,15 +4,15 @@
 //   value       = aws_eip.b-h_eip.public_ip
 // }
 
-output "Instance_ID" {
-  description = "Contains Instance id"
-  value       = aws_instance.b-h.id
-}
+// output "Instance_ID" {
+//   description = "Contains Instance id"
+//   value       = aws_instance.b-h.id
+// }
 
-output "VPC_ID" {
-  description = "Contains VPC id"
-  value       = data.aws_vpc.default.id
-}
+// output "VPC_ID" {
+//   description = "Contains VPC id"
+//   value       = data.aws_vpc.default.id
+// }
 
 ############ Adding VPC Outputs
 
