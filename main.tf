3d7d3b8512328a531a0f8de5c5c33861882e67af
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-09 15:14:39 -0500

    update userdata.sh#4

--- main.tf
+++ main.tf
@@ -63,11 +63,11 @@ resource "aws_s3_bucket" "upload_bucket" {
 // }
 
 # upload file to s3
-resource "aws_s3_bucket_object" "upload_object" {
-  bucket = "upload-bucket-for-ssm"
-  key    = "user_add.yml"
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("user_add.yml")
-}
+// resource "aws_s3_bucket_object" "upload_object" {
+//   bucket = "upload-bucket-for-ssm"
+//   key    = "user_add.yml"
+//   # The filemd5() function is available in Terraform 0.11.12 and later
+//   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+//   # etag = "${md5(file("path/to/file"))}"
+//   etag = filemd5("user_add.yml")
+// }
