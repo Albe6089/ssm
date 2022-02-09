874bcfa46331c3ac0fb01353ebd1a8ac366595e6
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-09 15:38:51 -0500

    file upload but no text#4

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_s3_bucket" "upload_bucket" {
 resource "aws_s3_bucket_object" "upload_object" {
   bucket = "upload-bucket-for-ssm"
   key    = "user_add"
-  source = "myfiles/user_add.yml"
+  source = "~/pipeline/ssm/user_add.yml"
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
