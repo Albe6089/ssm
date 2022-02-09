6f34dcf0f893747d13c0c29fbe19fca2171ac171
Albe6089 <89422682+Albe6089@users.noreply.github.com>
2022-02-09 15:54:49 -0500

    file upload but no text#7

--- main.tf
+++ main.tf
@@ -70,5 +70,5 @@ resource "aws_s3_bucket_object" "upload_object" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = "${filemd5("${path.module}/user_add.yml")}"
+  etag = filemd5("${path.module}/user_add.yml")
 }
